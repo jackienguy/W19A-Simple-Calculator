4dba15a1cd713903907582b1f778cb7180a37454
Your Name <you@example.com>
2021-09-21 20:55:21 -0600

    added inputs for user to enter operation and two numbers to complete calculation. Also converted input to integer by putting input function into int function

--- app.py
+++ app.py
@@ -3,3 +3,16 @@ from subtraction import subtract_two_numbers
 from multiplication import multiply_two_numbers
 from division import divide_two_numbers
 
+operation = input("Enter math operation (+, -, *, /):")
+num1 = int(input('Enter your first number: '))
+num2 = int(input('Enter your second number '))
+
+
+if operation == '+':
+    print (add_two_numbers(num1, num2))
+elif operation == '-':
+    print (subtract_two_numbers(num1, num2))
+elif operation == '*':
+    print (multiply_two_numbers(num1, num2))
+elif operation == "/":
+    print (divide_two_numbers(num1, num2))
